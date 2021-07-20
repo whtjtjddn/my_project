a0243f490c01972b81565b4cee88e815c5b784a7
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2021-07-20 14:19:19 +0900

    Localization
    언어 변경 코드( 한국어 --> 영어)  이런식

---
+++ LocalizedImage.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedImage : MonoBehaviour {
+
+    public static LocalizedImage Instance;
+
+    [SerializeField]
+    public string key;
+
+    private Image print_img;
+    [SerializeField]
+    private Image img_kor;
+    [SerializeField]
+    private Image img_eng;
+    
+    void Start () 
+    {
+        if (Instance == null) {
+            
+            Instance = this;
+        
+        } else if (Instance != this)
+        {
+            Destroy (gameObject);
+        }
+        DontDestroyOnLoad (gameObject);
+    
+        print_img.sprite = img_kor.sprite;
+    }
+
+    private void OnEnable()
+    {
+        print_img = GetComponent<Image> ();
+        img_kor = img_kor.GetComponent<Image>();
+        img_eng = img_eng.GetComponent<Image>();
+        ReloadImg();
+    }
+
+    public void ReloadImg()
+    {
+        if(key.Equals("kor"))
+        {
+            print_img.sprite= img_kor.sprite;
+        }
+        if(key.Equals("eng"))
+        {
+            print_img.sprite = img_eng.sprite;
+        }
+    }
+    void OnValidate()
+    {
+        ReloadImg();
+    }
+    
+}
---
+++ LocalizedText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour {
+
+    [SerializeField]
+    private string key;
+    private Text Text;
+    private TextMeshProUGUI TextPro;
+
+    // Use this for initialization
+    void Start () 
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        Text = GetComponent<Text> ();
+        TextPro = GetComponent<TextMeshProUGUI> ();
+        ReloadText();
+    }
+
+    public void ReloadText()
+    {
+        if (Text != null)
+        {
+            Text.text = LocalizationManager.Instance.GetLocalizedValue (key);
+        }
+        if (TextPro != null)
+        {
+            TextPro.text = LocalizationManager.Instance.GetLocalizedValue (key);
+        }
+    }
+
+}
